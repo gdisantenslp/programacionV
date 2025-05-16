82084bdb3a2caa409d764d84be1e1413890180ec
gabriel.disante <gabriel.disante@autoscheduler.ai>
2025-05-16 20:24:57 -0300

    Clase 15 de mayo

--- LoginMVCClase/LoginMVCClase/Controllers/UsuarioController.cs
+++ LoginMVCClase/LoginMVCClase/Controllers/UsuarioController.cs
@@ -1,6 +1,8 @@
 ﻿using LoginMVCClase.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+
 
 namespace LoginMVCClase.Controllers
 {
@@ -11,36 +13,89 @@ namespace LoginMVCClase.Controllers
             return View();
         }
 
+        string connectionString = @"Server=localhost\SQLEXPRESS;Database=ProgrammingV;Trusted_Connection=True;TrustServerCertificate=True;";
+
+        public ActionResult Lista()
+        {
+
+            List<UsuarioModel> usuarios = new List<UsuarioModel>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString)) {
+
+                string query = "SELECT id, username, name, fechanacimiento, email, password from Usuarios";
+            
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read()) {
+
+                    usuarios.Add(new UsuarioModel
+                    {
+                        id = reader.GetInt32(0),
+                        username = reader.GetString(1),
+                        name = reader.GetString(2),
+                        fechanacimiento = Convert.ToDateTime(reader.GetString(3)),
+                        email = reader.GetString(4),
+                        password    = reader.GetString(5)
+
+
+
+                    });
+                
+                }
+                return View(usuarios);
+
+            }
+                
+
+
+
+            return View();
+        }
+
+
+
         public ActionResult ForMethod(string username, string password, string name, string fechanacimiento, string email)
         {
-            UsuarioModel usuario;
-            List<UsuarioModel> listUsuario = new List<UsuarioModel>();
 
-            if (password == "password")
+            try
             {
-                ViewBag.Error = "ERROR";
-            }
-            else
-            {
-
-            
-                for (int i = 0; i <= 5; i++)
-
+                using (SqlConnection connection = new SqlConnection(connectionString))
                 {
-                    usuario = new UsuarioModel();
-                    usuario.email = "gabrieldisante@hotmail.com";
-                    usuario.username = "gdisante";
-                    usuario.password = "password";
-                    usuario.fechanacimiento = DateTime.Now;
-                    usuario.name = "Gbariel Di Sante";
-                    usuario.id = i;
+                    string query = @"INSERT INTO Usuarios (username, name, fechanacimiento, email, password) " +
+                        " VALUES (@username, @name, @fechanacimiento,@email,@password)  ";
 
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@username", username);
+                    command.Parameters.AddWithValue("@name", name);
+                    command.Parameters.AddWithValue("@fechanacimiento", fechanacimiento);
+                    command.Parameters.AddWithValue("@email", email);
+                    command.Parameters.AddWithValue("@password", password);
 
-                    listUsuario.Add(usuario);
+                    connection.Open();
+                    int rowsAffected = command.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+
+                        ViewBag.Mensaje = "Usuario Creado exitosamente!";
+                    }
+                    else
+                    {
+                        ViewBag.Mensaje = "Error - Usuario no creado";
+                    }
+
 
                 }
-            ViewBag.ListaUsuarios = listUsuario;
+
             }
+            catch (Exception ex) { 
+            
+                ViewBag.Error  = "Error al insertar el usuario " + username + ": " + ex.Message;
+
+            }
+
 
             return View("Index");
 
